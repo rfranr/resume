11ea9aa603e06df45e29a5a287f11f9cc90cada3
flopez <flopez@axpe.com>
2021-12-21 15:28:49 +0100

    deploy

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 git rm -rf resume.html
-pandoc '.\resume.md' -f markdown -t html -s -o resume.html
+pandoc '.\resume.md' -f markdown -t html -s -o static/resume.html
 git checkout docs
-git rm -rf resume.html
-git checkout main -- resume.html
-git add resume.html
+git add -A
 git commit -m "deploy"
 git push
 git checkout main
-git rm -rf resume.html
-
