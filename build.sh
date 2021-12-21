e1d47cf4c1188938dd275d284461677755d8cd4a
flopez <flopez@axpe.com>
2021-12-21 15:43:48 +0100

    deploy

--- build.sh
+++ build.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
-mkdir static
-pandoc '.\resume.md' -f markdown -t html -s -o static/resume.html
-git checkout docs
-git add -A
+git add resume.md
 git commit -m "deploy"
-git push
+pandoc '.\resume.md' -f markdown -t html -s -o static/resume.html
+
+git add static/resume.html
+git commit -m "deploy"
+
+git checkout docs
+
+git checkout main static/resume.html
+
+git add -A
+
+git status
+
+git commit -m "deploy"
 git checkout main
-rm -rf static/
+
+git rm -rf static/
+
+git add -A
+
+git status
+
+git commit -m "deploy"
+
+git push 
+
