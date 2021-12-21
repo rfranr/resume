d7c5ff7e185b1a8e28a824b7692dcef5acba46ca
flopez <flopez@axpe.com>
2021-12-21 16:15:10 +0100

    build

--- build.sh
+++ build.sh
@@ -6,13 +6,16 @@ echo "*" > ./static/.gitignore
 
 pandoc '.\resume.md' -f markdown -t html -s -o static/resume.html
 
+current_dir=`pwd -P`
 
+#enter in a untracked dir 
 cd ./static
 
 git clone -b docs --single-branch https://github.com/rfranr/resume.git
 
 cp resume.html ./resume/resume.html
 
+#enter in a copy of branch docs
 cd resume
 
 git status
@@ -22,6 +25,7 @@ git commit -m "deploy"
 
 git push
 
-cd ../..
+# goto working dir
+cd $current_dir
 
 rm -rf ./static
