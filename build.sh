fb77017ff93290dc1bc01b539c00c45e6f3acd4e
flopez <flopez@axpe.com>
2021-12-21 15:15:40 +0100

    build

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@ pandoc '.\resume.md' -f markdown -t html -s -o resume.html
 git checkout docs
 git rm -rf resume.html
 git checkout main -- resume.html
+git add resume.html
 git commit -m "deploy"
+git push
 git checkout main
 git rm -rf resume.html
 
